20801eaf36f27cf057ce5f6dcf0c1b94777852df
Hugo Müller-Downing <github@hu.md>
2023-03-22 10:57:49 +1100

    Improves generated documentation
    - Adds module.md for top-level package and module documentation
    - Adds Quiver logo to documentation index page
    - Sets source links to point to GitHub

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+import java.net.URL
+
 plugins {
   `java-library`
 }
@@ -19,4 +22,29 @@ dependencies {
   testRuntimeOnly(libs.junitEngine)
 
   apply(plugin = libs.plugins.dokka.get().pluginId)
+}
+
+// Copies Quiver logo into Dokka output directory, making images accessible in documentation
+tasks.register<Copy>("copyDocumentationImages") {
+  from("../images/quiver-logo-01.svg")
+  into("${getRootDir()}/lib/build/dokka/html/doc-images")
+}
+
+tasks.withType<DokkaTask>().configureEach {
+  dependsOn("copyDocumentationImages")
+  dokkaSourceSets {
+    named("main") {
+      moduleName.set("Quiver Library")
+
+      // Includes custom documentation
+      includes.from("module.md")
+
+      // Points source links to GitHub
+      sourceLink {
+        localDirectory.set(file("src/main/kotlin"))
+        remoteUrl.set(URL("https://github.com/cashapp/quiver/tree/master/lib/src/main/kotlin"))
+        remoteLineSuffix.set("#L")
+      }
+    }
+  }
 }
---
+++ lib/module.md
@@ -0,0 +1,25 @@
+# Module Quiver Library
+
+![](/doc-images/quiver-logo-01.svg)
+
+Quiver is a collection of extension methods and handy functions to make the wonderful functional programming Kotlin library, Arrow, even better.
+
+
+# Package app.cash.quiver
+Custom types (e.g. [`Outcome`](app.cash.quiver.Outcome)
+
+# Package app.cash.quiver.continuations
+Continuations for working with custom types (e.g. [`Outcome`](app.cash.quiver.Outcome))
+
+```kotlin
+val outcome1: Outcome<String, Int> = Present(1)
+val outcome2: Outcome<String, Int> = Present(2)
+outcome {
+  val a = outcome1.bind()
+  val b = outcome2.bind()
+  a + b
+}
+```
+
+# Package app.cash.quiver.extensions
+Extensions of built-in types (e.g. `Either`, `Option`, etc)
