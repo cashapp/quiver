7d2ffc4e0c2294cf3112cbd3bcb22dd2c7dee674
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-03-25 10:04:38 +0100

    Fix nested nullable issue

--- lib/src/main/kotlin/app/cash/quiver/extensions/Map.kt
+++ lib/src/main/kotlin/app/cash/quiver/extensions/Map.kt
@@ -1,8 +1,12 @@
 package app.cash.quiver.extensions
 
+import arrow.core.None
 import arrow.core.Option
+import arrow.core.Some
 
 /**
  * Extension function to get an Option from a nullable object on a map.
  */
-fun <K, A> Map<K, A>.getOption(k: K): Option<A> = Option.fromNullable(this[k])
+@Suppress("UNCHECKED_CAST")
+fun <K, A> Map<K, A>.getOption(k: K): Option<A> =
+  if (containsKey(k)) Some(get(k) as A) else None
