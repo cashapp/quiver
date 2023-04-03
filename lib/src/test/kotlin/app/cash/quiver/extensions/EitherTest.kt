d031e52b0967709dac1a032861ed9f2c81690057
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-04-03 06:01:22 +0200

    Either, Nullable & Ior zip (#33)
    * Nullable.zip
    
    * Either.zip
    
    * Ior.zip
    
    * Fix build
    
    * Remove redundant flatMap, and make emptyCombine internal
    
    * apiDump
    
    * Fix Ior.zip test

--- lib/src/test/kotlin/app/cash/quiver/extensions/EitherTest.kt
+++ lib/src/test/kotlin/app/cash/quiver/extensions/EitherTest.kt
@@ -12,6 +12,10 @@ import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.assertions.arrow.core.shouldBeSome
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arrow.core.either
+import io.kotest.property.checkAll
 import org.junit.jupiter.api.assertThrows
 
 class EitherTest : StringSpec({
@@ -81,6 +85,36 @@ class EitherTest : StringSpec({
   "validateNotNull is left if value is null - with label" {
     val value: String? = null
     value.validateNotNull("label".some()).shouldBeLeft(IllegalArgumentException("Value (`label`) should not be null"))
+  }
+  "zip2" {
+    checkAll(Arb.int(), Arb.int()) { a, b ->
+      a.right().zip(b.right()) { aa, bb -> aa + bb }.shouldBeRight(a + b)
+      a.right().zip(b.left()) { aa, bb: Int -> aa + bb }.shouldBeLeft(b)
+      a.left().zip(b.right()) { aa: Int, bb -> aa + bb }.shouldBeLeft(a)
+      a.left().zip(b.left()) { aa: Int, bb: Int -> aa + bb }.shouldBeLeft(a)
+    }
+  }
+  "zip10" {
+    checkAll(
+      Arb.either(Arb.int(), Arb.int()),
+      Arb.either(Arb.int(), Arb.int()),
+      Arb.either(Arb.int(), Arb.int()),
+      Arb.either(Arb.int(), Arb.int()),
+      Arb.either(Arb.int(), Arb.int()),
+      Arb.either(Arb.int(), Arb.int()),
+      Arb.either(Arb.int(), Arb.int()),
+      Arb.either(Arb.int(), Arb.int()),
+      Arb.either(Arb.int(), Arb.int()),
+      Arb.either(Arb.int(), Arb.int())
+    ) { a, b, c, d, e, f, g, h, i, j ->
+      val res = a.zip(b, c, d, e, f, g, h, i, j) { aa, bb, cc, dd, ee, ff, gg, hh, ii, jj ->
+        aa + bb + cc + dd + ee + ff + gg + hh + ii + jj
+      }
+      val expected = listOf(a, b, c, d, e, f, g, h, i, j).firstOrNull { it.isLeft() }
+        ?: listOf(a, b, c, d, e, f, g, h, i, j).mapNotNull { it.getOrNull() }.sum().right()
+
+      res shouldBe expected
+    }
   }
 })
 
--- testing-lib/src/main/kotlin/app/cash/quiver/arb/Arbitrary.kt
+++ testing-lib/src/main/kotlin/app/cash/quiver/arb/Arbitrary.kt
@@ -2,10 +2,22 @@ package app.cash.quiver.arb
 
 import app.cash.quiver.Outcome
 import app.cash.quiver.toOutcome
+import arrow.core.Ior
+import arrow.core.leftIor
+import arrow.core.rightIor
 import io.kotest.property.Arb
+import io.kotest.property.arbitrary.bind
+import io.kotest.property.arbitrary.choice
 import io.kotest.property.arbitrary.map
 import io.kotest.property.arrow.core.either
 import io.kotest.property.arrow.core.option
 
 fun <E, A> Arb.Companion.outcome(error: Arb<E>, value: Arb<A>): Arb<Outcome<E, A>> =
   Arb.either(error, Arb.option(value)).map { it.toOutcome() }
+
+fun <E, A> Arb.Companion.ior(error: Arb<E>, value: Arb<A>): Arb<Ior<E, A>> =
+  Arb.choice(
+    error.map { it.leftIor() },
+    value.map { it.rightIor() },
+    Arb.bind(error, value) { e, a -> Ior.Both(e, a) }
+  )
