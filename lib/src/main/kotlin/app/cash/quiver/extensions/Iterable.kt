1bc8e3deea21c1868dc7b4b7cb677e87131478e9
Jishan Lu <jishan@squareup.com>
2023-06-15 13:53:52 +0800

    move traverse to Iterable.kt

--- lib/src/main/kotlin/app/cash/quiver/extensions/Iterable.kt
+++ lib/src/main/kotlin/app/cash/quiver/extensions/Iterable.kt
@@ -1,4 +1,11 @@
 package app.cash.quiver.extensions
 
-class Iterable {
-}
+import arrow.core.Either
+import arrow.core.raise.either
+
+/**
+ * Returns an Either of a list of B results of applying the given transform function
+ * to each element(A) in the original collection.
+ */
+inline fun <E, A, B> Iterable<A>.traverse(f: (A) -> Either<E, B>): Either<E, Iterable<B>> =
+  let { l -> either { l.map { f(it).bind() } } }
