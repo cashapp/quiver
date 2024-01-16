4a3d3adbd549016a541777ecdf2a08fb5b55e9fa
Andrew Parker <aparker@squareup.com>
2024-01-16 22:20:39 +0900

    Add Iterable.traverse variations

--- lib/src/test/kotlin/app/cash/quiver/extensions/TraverseTest.kt
+++ lib/src/test/kotlin/app/cash/quiver/extensions/TraverseTest.kt
@@ -1,9 +1,13 @@
 package app.cash.quiver.extensions
 
 import arrow.core.Either
+import arrow.core.None
+import arrow.core.Some
 import io.kotest.assertions.arrow.core.shouldBeLeft
 import io.kotest.assertions.arrow.core.shouldBeRight
+import io.kotest.assertions.arrow.core.shouldBeSome
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
 class TraverseTest : StringSpec({
   "traverse a list of integers returns a Right of the list of mapped strings" {
@@ -31,5 +35,38 @@ class TraverseTest : StringSpec({
       if (it < 3) Either.Right(it) else Either.Left(it)
     }
     result shouldBeLeft 4
+  }
+
+  "traverseEither a list of integers returns a Right of the list of mapped strings" {
+    val result = listOf(1, 2, 3).traverseEither { Either.Right(it.toString()) }
+    result shouldBeRight listOf("1", "2", "3")
+  }
+
+  "traverse a list of integers returns a Some of the list of mapped strings" {
+    val result = listOf(1, 2, 3).traverse { Some(it.toString()) }
+    result shouldBeSome listOf("1", "2", "3")
+  }
+
+  "traverse a set of integers returns a Some of the list of mapped strings" {
+    val result = setOf(1, 2, 3).traverse { Some(it.toString()) }
+    result shouldBeSome listOf("1", "2", "3")
+  }
+
+  "traverse an empty list returns a Some of an empty list" {
+    val result = emptyList<Int>().traverse { Some(it.toString()) }
+    result shouldBeSome emptyList()
+  }
+
+  "traverse a list of integers returns a None if one of the mapped values is None" {
+    val result = listOf(1, 2, 3).traverse {
+      if (it % 2 == 0) None
+      else Some(it.toString())
+    }
+    result shouldBe None
+  }
+
+  "traverseOption a list of integers returns a Some of the list of mapped strings" {
+    val result = listOf(1, 2, 3).traverseOption { Some(it.toString()) }
+    result shouldBeSome listOf("1", "2", "3")
   }
 })
