d3240c5ec0283ae99c869816e711ddf65fa27a19
Hugo Müller-Downing <hugom@squareup.com>
2024-05-21 17:40:41 +1000

    Uses the Raise DSL instead of continuations
    Continuations have been deprecated in favour of the Raise DSL.
    
    I have removed OutcomeEagerEffect scope, as the Raise DSL does not
    expose an eager implementation to be extend. Instead, the expectation is
    that the caller wraps calls in `eagerEffect`. This binds the suspended
    function to an eager context.

--- lib/src/main/kotlin/app/cash/quiver/continuations/OutcomeEagerEffectScope.kt
+++ lib/src/main/kotlin/app/cash/quiver/continuations/OutcomeEagerEffectScope.kt
@@ -1,58 +1,42 @@
-@file:Suppress("DEPRECATION")
-
 package app.cash.quiver.continuations
 
-import arrow.core.Either
-import arrow.core.continuations.EagerEffectScope
-import arrow.core.continuations.EffectScope
-import arrow.core.continuations.eagerEffect
-import arrow.core.continuations.effect
-import arrow.core.left
-import arrow.core.merge
-import arrow.core.right
 import app.cash.quiver.Absent
 import app.cash.quiver.Failure
 import app.cash.quiver.Outcome
 import app.cash.quiver.Present
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.merge
+import arrow.core.raise.Raise
+import arrow.core.raise.eagerEffect
+import arrow.core.raise.effect
+import arrow.core.raise.fold
+import arrow.core.right
 
+// TODO(hugom): move this into an effect package, instead of continuations to align with arrow (?)
 @JvmInline
-value class OutcomeEagerEffectScope<E>(private val cont: EagerEffectScope<Either<Failure<E>, Absent>>) :
-  EagerEffectScope<Either<Failure<E>, Absent>> {
+value class OutcomeEffectScope<E>(private val cont: Raise<Either<Failure<E>, Absent>>) :
+  Raise<Either<Failure<E>, Absent>> {
 
   suspend fun <B> Outcome<E, B>.bind(): B =
     when (this) {
-      is Absent -> shift(Absent.right())
-      is Failure -> shift(this.left())
+      Absent -> raise(Absent.right())
+      is Failure -> raise(this.left())
       is Present -> value
     }
 
-  @Suppress("ILLEGAL_RESTRICTED_SUSPENDING_FUNCTION_CALL")
-  override suspend fun <B> shift(r: Either<Failure<E>, Absent>): B = cont.shift(r)
-}
-
-@JvmInline
-value class OutcomeEffectScope<E>(private val cont: EffectScope<Either<Failure<E>, Absent>>) :
-  EffectScope<Either<Failure<E>, Absent>> {
-
-  public suspend fun <B> Outcome<E, B>.bind(): B =
-    when (this) {
-      Absent -> shift(Absent.right())
-      is Failure -> shift(this.left())
-      is Present -> value
-    }
-
-  override suspend fun <B> shift(r: Either<Failure<E>, Absent>): B =
-    cont.shift(r)
+  override fun raise(r: Either<Failure<E>, Absent>): Nothing = cont.raise(r)
 }
 
 @Suppress("ClassName")
 object outcome {
-  inline fun <E, A> eager(crossinline f: suspend OutcomeEagerEffectScope<E>.() -> A): Outcome<E, A> =
+  inline fun <E, A> eager(crossinline f: OutcomeEffectScope<E>.() -> A): Outcome<E, A> =
     eagerEffect {
-      @Suppress("ILLEGAL_RESTRICTED_SUSPENDING_FUNCTION_CALL")
-      f(OutcomeEagerEffectScope(this))
+      f.invoke(OutcomeEffectScope<E>(this))
     }.fold({ it.merge() }, ::Present)
 
   suspend inline operator fun <E, A> invoke(crossinline f: suspend OutcomeEffectScope<E>.() -> A): Outcome<E, A> =
-    effect { f(OutcomeEffectScope(this)) }.fold({ it.merge() }, ::Present)
+    effect {
+      f.invoke(OutcomeEffectScope<E>(this))
+    }.fold({ it.merge() }, ::Present)
 }
